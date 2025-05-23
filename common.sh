63d00c9cf4d717d9096f4da789ab3ed5dcfe7f23
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-24 01:11:42 +0530

    Code optimization of mongodb

--- common.sh
+++ common.sh
@@ -10,6 +10,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)       #It will split the scriptName and gives only 10-logs which is field 1
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo -e "$M Script executing at : $N $(date)"  | tee -a $LOG_FILE
